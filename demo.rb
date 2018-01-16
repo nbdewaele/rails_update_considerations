45989545fd0f0ce7bf8804db165c31b37fe3d748
Nate DeWaele <nbdewaele@gmail.com>
2018-01-15 20:16:52 -0500

    initial

---
+++ demo.rb
@@ -0,0 +1,20 @@
+# TAKE AWAY - update to 2.5
+
+
+#Bundle update any csv files
+# 1.
+# tap - useful for calling someone elses method
+# 2.
+# slice -  grabs keys specified
+# below code incomplete
+# field_names.map do |fn|
+# 	fn.slice
+# end
+# 3.
+# transform_keys - maps keys to strings
+# 4.
+# Struct.new
+# point = Struct.new(:x, :y, :color)
+#
+#
+# get_stations.rb - in bryces github makes scraping easy
