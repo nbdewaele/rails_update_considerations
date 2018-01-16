aadc34f71c867705acb76203616eb5628aceb741
Nate DeWaele <nbdewaele@gmail.com>
2018-01-15 20:19:14 -0500

    links added

--- demo.rb
+++ demo.rb
@@ -18,3 +18,12 @@
 #
 #
 # get_stations.rb - in bryces github makes scraping easy
+
+# amberframework.org
+https://tenderlovemaking.com/2013/05/21/one-danger-of-freedom-patches.html
+https://mensfeld.pl/2017/12/ruby-2-5-0-upgrade-remarks/
+https://github.com/chrisccerami/mars-photo-api
+https://github.com/bkerley?tab=repositories
+https://mastodon.social/tags/programming
+http://ruby-doc.org/stdlib-2.0.0/libdoc/pp/rdoc/PP.html
+https://www.ruby-lang.org/en/news/2017/12/25/ruby-2-5-0-released/
